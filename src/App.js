f811be5f355929d0d8ecc4aa4bf29d5140c84a98
hanMaster <w54661c@gmail.com>
2020-01-28 01:47:36 +1000

    First version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CountDown from './components/CountDown/CountDown';
 
-function App() {
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h3>Отсчет до 30 января 12:00:00</h3>
+      <CountDown date="2020-01-30 12:00:00" />
+    </>
+  )
 }
 
-export default App;
---
+++ src/components/CountDown/CountDown.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import moment from "moment";
+import classes from "./CountDown.module.css";
+
+class CountDown extends Component {
+  state = {
+    untillDateTime: moment(this.props.date),
+    diffDateTime: {
+      days: 0,
+      hours: 0,
+      mins: 0,
+      seconds: 0
+    },
+    timer: null
+  };
+
+  leadZero = num => {
+    let temp = num + "";
+    if (temp.length === 1) {
+      return "0" + temp;
+    }
+    return temp;
+  };
+
+  calcDiff = () => {
+    const fromDate = moment();
+    const diff = this.state.untillDateTime.diff(fromDate);
+
+    const diffDateTime = {
+      days: this.leadZero(Math.floor(diff / 1000 / 60 / 60 / 24)),
+      hours: this.leadZero(Math.floor((diff / 1000 / 60 / 60) % 24)),
+      mins: this.leadZero(Math.floor((diff / 1000 / 60) % 60)),
+      seconds: this.leadZero(Math.floor((diff / 1000) % 60))
+    };
+
+    this.setState({ diffDateTime });
+  };
+
+  componentDidMount() {
+    const timer = setInterval(() => {
+      this.calcDiff();
+    }, 1000);
+    this.setState({ timer });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.timer);
+  }
+
+  render() {
+    const { days, hours, mins, seconds } = this.state.diffDateTime;
+    return (
+      <div className={classes.CountDown}>
+        <div className={classes.section}>
+          <div className={classes.column}>
+            <span className={classes.days}>{days}</span>
+            <span className={classes.title}>Дни</span>
+          </div>
+        </div>
+
+        <div className={classes.section}>
+          <div className={classes.column}>
+            <span className={classes.separator}>:</span>
+            <span className={classes.titleSeparator}></span>
+          </div>
+        </div>
+
+        <div className={classes.section}>
+          <div className={classes.column}>
+            <span className={classes.hours}>{hours}</span>
+            <span className={classes.title}>Часы</span>
+          </div>
+        </div>
+
+        <div className={classes.section}>
+          <div className={classes.column}>
+            <span className={classes.separator}>:</span>
+            <span className={classes.titleSeparator}></span>
+          </div>
+        </div>
+
+        <div className={classes.section}>
+          <div className={classes.column}>
+            <span className={classes.mins}>{mins}</span>
+            <span className={classes.title}>Мин</span>
+          </div>
+        </div>
+
+        <div className={classes.section}>
+          <div className={classes.column}>
+            <span className={classes.separator}>:</span>
+            <span className={classes.titleSeparator}></span>
+          </div>
+        </div>
+
+        <div className={classes.section}>
+          <div className={classes.column}>
+            <span className={classes.seconds}>{seconds}</span>
+            <span className={classes.title}>Сек</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CountDown;
